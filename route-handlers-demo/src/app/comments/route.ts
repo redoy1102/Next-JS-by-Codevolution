59329797f89ba57dc17dca7ac76e658ea30546da
redoy1102 <hridoyvlog9@gmail.com>
2024-12-23 00:55:56 +0600

    Check out

---
+++ route-handlers-demo/src/app/comments/route.ts
@@ -0,0 +1,25 @@
+import {comments} from "./data";
+
+export async function GET()
+{
+    return Response.json(comments);
+}
+
+export async function POST(request: Request){
+    // Getting the comment from the user post. We need convert the comment in json format as user post data always be converted into stringify.
+    const comment = await request.json();
+
+    // Creating the new comment by the user's comment to add in the comments array
+    const newComment = {
+        id: comments.length + 1,
+        text: comment.text,
+    };
+    comments.push(newComment);
+
+    return new Response(JSON.stringify(newComment), {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        status: 201,
+    });
+}
